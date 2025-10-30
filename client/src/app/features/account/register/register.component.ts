9e75176b2ac268993de1d5db32f8ffbecd63903c
Ulysses Garsin <usgarsin@gmail.com>
2025-10-31 00:28:07 +0800

    Next

--- client/src/app/features/account/register/register.component.ts
+++ client/src/app/features/account/register/register.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../../../core/services/snackbar.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormField,
+    MatLabel,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  private fb = inject(FormBuilder);
+  private accountService = inject(AccountService);
+  private router = inject(Router);
+  private snack = inject(SnackbarService);
 
+  registerForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    email: [''],
+    password: ['']
+  });
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => {
+        this.snack.success("Registration is successful, you can now login");
+        this.router.navigateByUrl("/account/login");
+      }
+    })
+  }
 }
